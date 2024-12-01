a88d32d3b826299e057ba57712592921de4daf40
Nour Elfangary <noureldinesamehelfangary@gmail.com>
2024-12-01 18:45:24 +0000

    Added an offset to the trail

--- Assets/Scripts/DrawTrail.cs
+++ Assets/Scripts/DrawTrail.cs
@@ -1,27 +1,41 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class DrawTrail : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject player;
-    public float trailHeight = 0.2f;
-    public float segmentDuration = 1f;
+    public float trailHeight = 0.1f;
+    public float segmentDuration = 0.25f;
+    public float offset = 2f;
 
     private LineRenderer _line;
+    private readonly Queue<Vector3> _positionQueue = new Queue<Vector3>();
     
-    void Start()
+    private void Start()
     {
         _line = GetComponent<LineRenderer>();
+        _line.positionCount = 1;
+        
         _line.SetPosition(0, new Vector3(player.transform.position.x, trailHeight, player.transform.position.z));
         
-        InvokeRepeating(nameof(AddSegment), 0f, segmentDuration);
+        InvokeRepeating(nameof(QueuePosition), 0f, segmentDuration);
+        InvokeRepeating(nameof(AddSegment), offset, segmentDuration);
     }
 
-    void AddSegment()
+    private void AddSegment()
     {
+        if (_positionQueue.Count == 0)
+        {
+            return;
+        }
+
         _line.positionCount += 1;
-        _line.SetPosition(_line.positionCount - 1, new Vector3(player.transform.position.x, trailHeight, player.transform.position.z));
+        _line.SetPosition(_line.positionCount - 1, _positionQueue.Dequeue());
+
+    }
+
+    private void QueuePosition()
+    {
+        _positionQueue.Enqueue(new Vector3(player.transform.position.x, trailHeight, player.transform.position.z));
     }
 }
