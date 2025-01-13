fce65783d91f5d0c09830ea0e717c82fb4f538b9
Nour Elfangary <noureldinesamehelfangary@gmail.com>
2025-01-13 15:03:59 +0000

    Scene setup generation and power up spawns on the floor

--- Assets/Scripts/FaceCamera.cs
+++ Assets/Scripts/FaceCamera.cs
@@ -1,8 +1,17 @@
+using Oculus.Interaction;
 using UnityEngine;
 
 public class FaceCamera : MonoBehaviour
 {
     public Transform player;
+
+    void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        }
+    }
 
     void Update()
     {
