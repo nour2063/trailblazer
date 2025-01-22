91f61e3fc9aa4c7d78dac1d2d85ed6b36e5bb73d
Nour Elfangary <noureldinesamehelfangary@gmail.com>
2025-01-22 17:57:32 +0000

    added a game script to handle the start trigger and sound effects

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -9,6 +10,14 @@ public class Game : MonoBehaviour
     public float timer = 100f;
     public GameObject gameScripts;
     public TextMeshProUGUI scoreText;
+    public float multTimer = 10f;
+
+    public AudioClip coinSound;
+    public AudioClip multSound;
+    public AudioClip timeBonusSound;
+    public AudioClip boostSound;
+
+    private int mult = 1;
     
     void StartAfterDelay()
     {
@@ -19,35 +28,46 @@ public class Game : MonoBehaviour
     {
         if (other.CompareTag("Trigger"))
         {
+            Destroy(other.gameObject);
             Invoke(nameof(StartAfterDelay), 3f);
         }
         else if (other.CompareTag("Coin"))
         {
-            score += 1;
-            UpdateScore();
+            UpdateScore(1*mult);
             Destroy(other.gameObject);
         } else if (other.CompareTag("3Coin"))
         {
-            score += 3;
-            UpdateScore();
+            UpdateScore(3*mult);
             Destroy(other.gameObject);
         } else if (other.CompareTag("TimeBonus"))
         {
             timer += 10;
+            GetComponent<AudioSource>().PlayOneShot(timeBonusSound);
             Destroy(other.gameObject);
         } else if (other.CompareTag("Boost"))
         {
             // todo make boost work
+            GetComponent<AudioSource>().PlayOneShot(boostSound);
             Destroy(other.gameObject);
         } else if (other.CompareTag("Multiplier"))
         {
-            // todo make multiplier
+            GetComponent<AudioSource>().PlayOneShot(multSound);
+            StartCoroutine(Multiplier());
             Destroy(other.gameObject);
         }
     }
     
-    void UpdateScore()
+    void UpdateScore(int change)
     {
-        scoreText.text = "score: " + score;
+        score += change * mult;
+        scoreText.text = "" + score;
+        GetComponent<AudioSource>().PlayOneShot(coinSound);
+    }
+
+    IEnumerator Multiplier()
+    {
+        mult = 2;
+        yield return new WaitForSeconds(multTimer);
+        mult = 1;
     }
 }
