45df767f03e39f60d86876c683b941bd4a81b787
Nour Elfangary <noureldinesamehelfangary@gmail.com>
2025-02-21 16:01:54 +0000

    Added particles

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AdaptivePerformance.VisualScripting;
 using UnityEngine.Rendering.UI;
 
 public class Game : MonoBehaviour
@@ -22,9 +23,14 @@ public class Game : MonoBehaviour
     public AudioClip timeBonusSound;
     public AudioClip boostSound;
 
-    public Material litGate;
+    public GameObject gate1;
+    public GameObject gate2;
+    public GameObject gate3;
+    public GameObject gate4;
+
+    public Material gateMaterial;
+    public Material baseMaterial;
     public Material startGate;
-    public Material litBase;
 
     private int _mult = 1;
     private bool _gameStarted = false;
@@ -40,53 +46,38 @@ public class Game : MonoBehaviour
     
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Gate1"))
+        if (other.CompareTag("Gate1") && _checkpoints == 0)
         {
-            Destroy(other.gameObject);
-            GetComponent<AudioSource>().PlayOneShot(startSound);
-            Invoke(nameof(StopAudio), 0.75f); 
-            _checkpoints++;
-
-            var gate = GameObject.FindGameObjectWithTag("Gate2");
-            gate.GetComponent<Renderer>().material = litGate;
-            gate.GameObject().transform.Find("Base").GetComponent<Renderer>().material = litBase;
-            gate.GameObject().transform.Find("Icon").GameObject().SetActive(true);
+            gate1.GetComponentInChildren<ParticleSystem>().Play();
+            other.gameObject.SetActive(false);
+            
+            EnableGate(gate2);
         }
         
         if (other.CompareTag("Gate2") && _checkpoints == 1)
         {
-            Destroy(other.gameObject);
-            GetComponent<AudioSource>().PlayOneShot(startSound);
-            Invoke(nameof(StopAudio), 0.75f); 
-            _checkpoints++;
-
-            var gate = GameObject.FindGameObjectWithTag("Gate3");
-            gate.GetComponent<Renderer>().material = litGate;
-            gate.GameObject().transform.Find("Base").GetComponent<Renderer>().material = litBase;
-            gate.GameObject().transform.Find("Icon").GameObject().SetActive(true);
+            gate2.GetComponentInChildren<ParticleSystem>().Play();
+            other.gameObject.SetActive(false);
+            
+            EnableGate(gate3);
         }
         
         if (other.CompareTag("Gate3") && _checkpoints == 2)
         {
-            Destroy(other.gameObject);
-            GetComponent<AudioSource>().PlayOneShot(startSound);
-            Invoke(nameof(StopAudio), 0.75f); 
-            _checkpoints++;
-
-            var gate = GameObject.FindGameObjectWithTag("Trigger");
-            gate.GetComponent<Renderer>().material = startGate;
-            gate.GameObject().transform.Find("Base").GetComponent<Renderer>().material = litBase;
-            gate.GameObject().transform.Find("Icon").GameObject().SetActive(true);
+            gate3.GetComponentInChildren<ParticleSystem>().Play();
+            other.gameObject.SetActive(false);
+            
+            EnableGate(gate4, final:true);
         }
 
-        if (other.CompareTag("Trigger") && _checkpoints == 3)
+        if (other.CompareTag("Gate4") && _checkpoints == 3)
         {
+            gate4.GetComponentInChildren<ParticleSystem>().Play();
             GetComponent<AudioSource>().clip = startSound;
             GetComponent<AudioSource>().time = 3f; 
             GetComponent<AudioSource>().Play();
             
-            Destroy(other.gameObject);
-            // Invoke(nameof(StartGame), delay);
+            other.gameObject.SetActive(false);
             StartGame();
         }
         else if (other.CompareTag("Coin"))
@@ -105,9 +96,10 @@ public class Game : MonoBehaviour
             timer += 5;
             GetComponent<AudioSource>().PlayOneShot(timeBonusSound);
         } 
+        // todo make boost work for multiplayer
         // else if (other.CompareTag("Boost"))
         // {
-        //     // todo make boost work for multiplayer
+        //     
         //     Destroy(other.gameObject);
         //     GetComponent<AudioSource>().PlayOneShot(boostSound);
         // } 
@@ -147,5 +139,16 @@ public class Game : MonoBehaviour
     void StopAudio()
     {
         GetComponent<AudioSource>().Stop();
+    }
+
+    void EnableGate(GameObject g, bool final = false)
+    {
+        GetComponent<AudioSource>().PlayOneShot(startSound);
+        Invoke(nameof(StopAudio), 0.75f); 
+        _checkpoints++;
+        var gate = g.transform.Find("Trigger");
+        gate.GetComponent<Renderer>().material = final ? startGate : gateMaterial;
+        gate.transform.Find("Base").GetComponent<Renderer>().material = baseMaterial;
+        gate.transform.Find("Icon").GameObject().SetActive(true);
     }
 }
